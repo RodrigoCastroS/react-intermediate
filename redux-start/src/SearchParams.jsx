692a41cd90d0d705e2530323bbee6e6457d3caf2
Rodrigo C <rodrigocsastre@gmail.com>
2024-06-20 17:26:55 +0200

    feat: use endpoints created removing the useContext and useState hooks

--- redux-start/src/SearchParams.jsx
+++ redux-start/src/SearchParams.jsx
@@ -1,23 +1,23 @@
-import { useContext, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import Results from "./Results";
-import AdoptedPetContext from "./AdoptedPetContext";
 import useBreedList from "./useBreedList";
-import fetchSearch from "./fetchSearch";
+import { useDispatch, useSelector } from "react-redux";
+import { all } from "./searchParamsSlice";
+import { useSearchQuery } from "./petAPIService";
+
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [requestParams, setRequestParams] = useState({
-    location: "",
-    animal: "",
-    breed: "",
-  });
-  const [adoptedPet] = useContext(AdoptedPetContext);
+  const dispatch = useDispatch();
+  const adoptedPet = useSelector((state) => state.adoptedPet.value);
+  const searchParams = useSelector((state) => state.searchParams.value);
+
+  let { data: pets } = useSearchQuery(searchParams);
+
+  pets = pets ?? [];
+
   const [animal, setAnimal] = useState("");
   const [breeds] = useBreedList(animal);
-
-  const results = useQuery(["search", requestParams], fetchSearch);
-  const pets = results?.data?.pets ?? [];
 
   return (
     <div className="search-params">
@@ -30,7 +30,7 @@ const SearchParams = () => {
             breed: formData.get("breed") ?? "",
             location: formData.get("location") ?? "",
           };
-          setRequestParams(obj);
+          dispatch(all(obj));
         }}
       >
         {adoptedPet ? (
